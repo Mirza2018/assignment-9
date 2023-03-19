af80d54124163323b76703be7959fc4a799f752b
Mirza2018 <mirza.eee.4th@gmail.com>
2023-03-19 09:55:17 +0600

    final3

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -30,10 +30,10 @@ const Home = () => {
               <a href="https://www.youtube.com/watch?v=_ItSHuIJAJ8&ab_channel=HackerEarth" className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
                 <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
                 <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                  <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                  <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                 </span>
                 <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                  <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                  <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                 </span>
                 <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Demo Video</span>
               </a>
@@ -46,7 +46,7 @@ const Home = () => {
 
 
       <h1 className='my-10 text-center text-rose-500 text-2xl font-black font-mono'>Reviews from buyers</h1>
-      <div className='grid grid-cols-3 gap-2 font-sans'>
+      <div className='grid md:grid-cols-3 gap-2 font-sans'>
         {
 
           reviwes.slice(0, 3).map(review => <Review review={review} key={review.id}></Review>)
--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -9,7 +9,7 @@ const Reviews = () => {
     return (
         <div >
             <h1 className='text-center text-rose-500 text-2xl font-black font-mono my-12'>Reviews from buyers</h1>
-            <div className='grid grid-cols-3 gap-2 font-sans'>
+            <div className='grid md:grid-cols-3 gap-2 font-sans'>
                 {
                     reviwes.map(review => <Review review={review} key={review.id}></Review>)
                 }
