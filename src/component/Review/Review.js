c648db3180114613e843e7a0eaafb152acd8f90d
Mirza2018 <mirza.eee.4th@gmail.com>
2023-03-19 07:37:19 +0600

    reviews

--- src/component/Review/Review.js
+++ src/component/Review/Review.js
@@ -3,11 +3,18 @@ import React from 'react';
 const Review = ({ review }) => {
     const { name, img, text, ratting } = review
     return (
-        <div>
-            <img src={img} alt="" />
-            <h1>{name}</h1>
-            <p>{text}</p>
-            <small>{ratting}</small>
+        <div className=''>
+            <div class="pt-5 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex flex-col items-center pb-10">
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={img} alt="Bonnie image" />
+                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">{text}</span>
+                    <div class="flex mt-4 space-x-3 md:mt-6">
+                        <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{ratting}</a>
+                       
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -7,11 +7,13 @@ const Reviews = () => {
 
     console.log(reviwes);
     return (
-        <div>
-            <h1>Review</h1>
-            {
-                reviwes.map(review =><Review review={review}></Review>)
-            }
+        <div >
+            <h1 className='text-center text-rose-500 text-2xl font-black font-mono'>Reviews from buyers</h1>
+            <div className='grid grid-cols-3 gap-2 font-sans'>
+                {
+                    reviwes.map(review => <Review review={review}></Review>)
+                }
+            </div>
         </div>
     );
 };
